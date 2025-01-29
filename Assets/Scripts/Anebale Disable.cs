c5c48fea21afc9581d7c856cd35a30f33e405c65
Alexander Papadopoulos <papadoal@shernet.sheridancollege.ca>
2025-01-29 10:28:04 -0500

    Made a bread and a chicken and things go away now

---
+++ Assets/Scripts/Anebale Disable.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnebaleDisable : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    public AnebaleDisable script;
+    public GameObject GO;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //sr.enabled = false;
+            //script.enabled = false;
+            GO.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //sr.enabled = true;
+            //script.enabled = true;
+            GO.SetActive(true);
+        }
+    }
+}
